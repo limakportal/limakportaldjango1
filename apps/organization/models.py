ab68484c7c527b709d32e1c7bc2904ea96eb3742
kayaserhat <kayaserhat@users.noreply.github.com>
2020-07-29 10:40:25 +0300

    migrate

--- apps/organization/models.py
+++ apps/organization/models.py
@@ -9,6 +9,5 @@ class Organization(models.Model):
     Name = models.CharField(blank=True, max_length=50,null=True)
     Status = models.ForeignKey(Status,on_delete=models.CASCADE)
     OrganizationType = models.ForeignKey(OrganizationType, on_delete=models.CASCADE)
-    # Organization = models.ForeignKey(Organization, on_delete = models.CASCADE, related_name='upperorganizationid')
-    
+    UpperOrganization = models.ForeignKey('self', on_delete = models.CASCADE)
     
