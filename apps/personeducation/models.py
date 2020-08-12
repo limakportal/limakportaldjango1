2463011a2f76c16da84a757b0077f7ec223d2a1e
emrah ince <emrahince@outlook.com>
2020-08-12 16:05:15 +0300

    sda

--- apps/personeducation/models.py
+++ apps/personeducation/models.py
@@ -6,12 +6,12 @@ class PersonEducation(models.Model):
     class Meta:
         db_table = 'PersonEducation'
 
-    PersonId = models.ForeignKey(Person,on_delete=models.CASCADE)
+    Person = models.ForeignKey(Person,on_delete=models.CASCADE)
     SchoolName = models.CharField(max_length = 100 , blank=True, null=True)
     SchoolTypeId = models.IntegerField(blank=True,null=True)
     StartDate = models.DateField(blank=True, null=True)
     GraduationDate = models.DateField(blank=True, null=True)
-    CityId = models.ForeignKey(City,on_delete=models.CASCADE,blank=True, null=True)
+    City = models.ForeignKey(City,on_delete=models.CASCADE,blank=True, null=True)
     Description = models.CharField(max_length = 200 ,blank=True, null=True)
     Degree = models.DecimalField(max_digits=5,decimal_places=2,blank=True, null=True)
     Faculty = models.CharField(max_length = 100,blank=True, null=True)
