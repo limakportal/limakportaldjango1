6dd737a99d7e10482962e999a5c80ba87a3274a8
emrah ince <emrahince@outlook.com>
2020-08-14 17:06:46 +0300

    dsadsa

--- apps/personhistory/models.py
+++ apps/personhistory/models.py
@@ -2,15 +2,12 @@ from django.db import models
 from apps.person.models import Person
 from apps.staff.models import Staff
 
-# Create your models here.
-
-
 class PersonHistory(models.Model):
     class Meta:
         db_table = 'PersonHistory'
 
     Person = models.ForeignKey(Person,on_delete=models.CASCADE)
-    Staff = models.ForeignKey(Staff,on_delete=models.CASCADE)
+    Staff = models.ForeignKey(Staff,on_delete=models.CASCADE,related_name='Staff')
     EntryDate = models.DateField(auto_now_add=True,max_length=50,blank=True,null=True)
     TerminationDate = models.DateField(auto_now_add=True,max_length=50,blank=True,null=True)
 
