ae6c87d08fa77ce55e910a1f6f2e422790dd063c
emrah ince <emrahince@outlook.com>
2020-08-17 13:38:39 +0300

    dede

--- apps/personbusiness/models.py
+++ apps/personbusiness/models.py
@@ -8,7 +8,7 @@ class PersonBusiness(models.Model):
     Person = models.ForeignKey(Person,on_delete=models.CASCADE)
     ContractType = models.IntegerField(blank=True,null=True)
     JobStartDate = models.DateField(blank=True, null=True)
-    FirstBudger = models.DecimalField(max_digits=10,decimal_places=2,blank=True, null=True)
+    FirstBudger = models.DecimalField(max_digits=8,decimal_places=2,blank=True, null=True)
     JobCode = models.CharField(max_length = 100 , blank=True, null=True)
     RegisterNo = models.CharField(max_length=100,blank=True,null=True)
     SGKRegisterNo = models.CharField(max_length=100,blank=True,null=True)
