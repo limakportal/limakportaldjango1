01bf060e9f013b3d5a1869770a76c12136efdd21
emrah ince <emrahince@outlook.com>
2020-08-18 13:50:11 +0300

    cors deneme

--- django_project/settings/base.py
+++ django_project/settings/base.py
@@ -94,7 +94,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'django_project.urls'
 
