ec4246a9b5f1d7409fba10e4e47f1af5b1cc9350
emrah ince <emrahince@outlook.com>
2020-08-18 16:14:19 +0300

    person model update

--- apps/person/models.py
+++ apps/person/models.py
@@ -12,7 +12,7 @@ class Person(models.Model):
     Surname = models.CharField(max_length=50,blank=True,null=True)
     IdentityID = models.CharField(max_length=50,blank=True,null=True)
     Nationality = models.ForeignKey(Nationality,on_delete=models.CASCADE,related_name='NationalityId',blank=True, null=True)
-    Address = models.CharField(max_length=50,blank=True,null=True)
+    Address = models.CharField(max_length=200,blank=True,null=True)
     Telephone = models.CharField(max_length=50,blank=True,null=True)
     State = models.IntegerField(blank=True,null=True)
     Email = models.CharField(max_length=50,blank=True,null=True)
