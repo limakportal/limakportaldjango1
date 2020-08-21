fc7ba261b281638852e6a800a8945645c56d3be0
emrah ince <emrahince@outlook.com>
2020-08-21 14:53:55 +0300

    vocation views edit

--- apps/vocationdays/views.py
+++ apps/vocationdays/views.py
@@ -26,7 +26,7 @@ class VocationDetails(APIView):
         def get_object(self,id):
             try:
                 return VocationDays.objects.get(id=id)
-            except Status.DoesNotExist:
+            except VocationDays.DoesNotExist:
                 return Response(status=status.HTTP_404_NOT_FOUND)
 
         def get(self, request, id):
@@ -37,7 +37,7 @@ class VocationDetails(APIView):
 
         def put(self, request,id):
             vocationDays = self.get_object(id)
-            serializer = StatusSerializer(vocationDays, data=request.data)
+            serializer = VocationDaysSerializer(vocationDays, data=request.data)
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data)
