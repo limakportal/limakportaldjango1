2961a4ea1548b9a69e3722230335157252c912e9
kayaserhat <kayaserhat@users.noreply.github.com>
2020-08-25 14:36:23 +0300

    üst yönetici durumu

--- apps/person/businesrules.py
+++ apps/person/businesrules.py
@@ -17,7 +17,19 @@ class PersonApproverDetails(APIView):
             staff = Staff.objects.get(Person = id)
             organization = Organization.objects.get(id = staff.Organization.id)
             if organization.CanApproveRight:
-                personel = Person.objects.get(id=id)
+                ystaff = Staff.objects.get(Organization=organization.id,Title = organization.ManagerTitle.id)
+                
+                if ystaff == None:
+                    return Response('Kişinin bağlı olduğum birimde yönetici bulunmamaktadır.',status=status.HTTP_404_NOT_FOUND)
+                personel = Person.objects.get(id=ystaff.Person.id)
+                if personel.id == id:
+                    staffs = Staff.objects.get(Organization = organization.UpperOrganization.id)
+                    personel = Person.objects.get(id=staffs.Person.id)
+                    serializer = PersonSerializer(personel)
+                    data = {}
+                    data['id'] = serializer.data['id']
+                    data['FullName'] = serializer.data['Name'] + ' ' + serializer.data['Surname']
+                    return Response(data)
                 serializer = PersonSerializer(personel)
                 data = {}
                 data['id'] = serializer.data['id']
