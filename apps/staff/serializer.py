18466762b627ac4bf26863772f5008bd48584f02
emrah ince <emrahince@outlook.com>
2020-08-25 12:56:28 +0300

    staff edit

--- apps/staff/serializer.py
+++ apps/staff/serializer.py
@@ -6,7 +6,7 @@ from apps.title.serializer import TitleSerializer
 from apps.role.serializer import RoleSerializer
 from apps.organization.models import Organization
 
-
+from ..title.models import Title
 from ..title.serializer import TitleSerializer
 from ..person.models import Person
 from ..organization.models import Organization
@@ -37,12 +37,45 @@ class OrganizationSerializer(serializers.ModelSerializer):
         fields = ('__all__')
 
 class StaffJoinSerializer(serializers.ModelSerializer):
-    Organization = OrganizationSerializer()
-    Person = PersonSerializer()
-    Title = TitleSerializer()
     class Meta:
         model = Staff
-        fields = ('__all__')
+        fields = (
+            'id',
+            'Organization',
+            'Person',
+            'Title',
+            'Role',
+            'PersonObj',
+            'TitleObj',
+            'OrganizationObj'
+        )
+    OrganizationObj = serializers.SerializerMethodField()
+    PersonObj = serializers.SerializerMethodField()
+    TitleObj = serializers.SerializerMethodField()
+
+    def get_PersonObj(self,obj):
+        try:
+            person = Person.objects.get(id=obj.Person_id)
+            serializer = PersonSerializer(person)
+            return serializer.data
+        except :
+            return None
+
+    def get_TitleObj(self,obj):
+        try:
+            title = Title.objects.get(id=obj.Title_id)
+            serializer = TitleSerializer(title)
+            return serializer.data
+        except :
+            return None
+
+    def get_OrganizationObj(self,obj):
+        try:
+            organization = Organization.objects.get(id=obj.Organization_id)
+            serializer = TitleSerializer(organization)
+            return serializer.data
+        except :
+            return None
 
 class StafForPersonSerializer(serializers.ModelSerializer):
     Title = TitleSerializer()
