ef732d10d4b536c2676aba96e669734f43c92975
emrah ince <emrahince@outlook.com>
2020-08-25 17:08:52 +0300

    role bussiness edit

--- apps/role/businesrules.py
+++ apps/role/businesrules.py
@@ -6,6 +6,7 @@ import json
 
 
 from ..role.models import Role
+from ..authority.models import Authority
 
 from ..role.serializer import RoleSerializer , RoleViewSerializer
 from ..authority.serializer import AuthoritySerializer
@@ -52,6 +53,9 @@ class RoleWithPermissionAPIView(APIView):
                         else:
                             transaction.savepoint_rollback(transaction)
                             return Response(userRoleSerializer.error_messages , status = status.HTTP_400_BAD_REQUEST)
+            
+            role = self.get_object(roleSerializer.data['id'])
+            roleSerializer = RoleViewSerializer(role)
             return Response(roleSerializer.data, status=status.HTTP_201_CREATED)
         else:
             transaction.savepoint_rollback(transaction)
@@ -68,5 +72,38 @@ class RoleWithPermissionDetails(APIView):
         role = self.get_object(id)
         serializer = RoleViewSerializer(role)
         return Response(serializer.data)
+
+    @transaction.atomic       
+    def put(self, request,id):
+        role = self.get_object(id)
+        serializer = RoleSerializer(role, data=request.data['role'])
+        if serializer.is_valid():
+            serializer.save()
+
+            authorityes = Authority.objects.filter(Role_id = id)
+            for authoritiy in authorityes:
+                authoritiy.delete()
+            
+            permissionRequestData = {}
+            permissionRequestData = request.data['permissions']
+            if len(permissionRequestData) > 0:
+                for permissionData in permissionRequestData:
+                    if 'id' in json.loads(json.dumps(permissionData)):
+                        data = {}
+                        data['Role'] = id
+                        data['Permission'] = permissionData['id']
+                        data['Active'] = 1   
+                        authoritySerializer = AuthoritySerializer(data = data)
+                        if authoritySerializer.is_valid():
+                            authoritySerializer.save()
+                        else:
+                            transaction.savepoint_rollback(transaction)
+                            return Response(authoritySerializer.error_messages , status = status.HTTP_400_BAD_REQUEST)
+
+            roleSerializer = RoleViewSerializer(role)
+            return Response(roleSerializer.data)
+        transaction.savepoint_rollback(transaction)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
         
 
