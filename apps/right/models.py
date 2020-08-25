e57039b7e8dd1a8b909e15c9d407ff6f88eda3bb
kayaserhat <kayaserhat@users.noreply.github.com>
2020-08-25 10:25:53 +0300

    rights yeni kolon

--- apps/right/models.py
+++ apps/right/models.py
@@ -20,6 +20,7 @@ class Right(models.Model):
     RightStatus = models.ForeignKey(RightStatus, on_delete = models.CASCADE,blank=True, null=True)
     RightNumber = models.IntegerField(blank=True,null=True)
     DenyExplanation = models.CharField(max_length=50,blank=True, null=True)
+    IKHasField = models.BooleanField(default=False)
 
     def __str__(self):
         return self.Person.Name
