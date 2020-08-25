cb1fc56739fcf613925a2fa1ddc7a2c9026ad495
emrah ince <emrahince@outlook.com>
2020-08-25 12:05:03 +0300

    staff edit

--- apps/staff/serializer.py
+++ apps/staff/serializer.py
@@ -7,6 +7,12 @@ from apps.role.serializer import RoleSerializer
 from apps.organization.models import Organization
 
 
+from ..title.serializer import TitleSerializer
+from ..person.models import Person
+from ..organization.models import Organization
+
+
+
 class OrganizationForStaffSerializer(serializers.ModelSerializer):
     class Meta:
         model = Organization
@@ -17,6 +23,23 @@ class OrganizationForStaffSerializer(serializers.ModelSerializer):
 
 
 class StaffSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Staff
+        fields = ('__all__')
+
+class PersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person       
+        fields = ('__all__')
+class OrganizationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organization
+        fields = ('__all__')
+
+class StaffJoinSerializer(serializers.ModelSerializer):
+    Organization = OrganizationSerializer()
+    Person = PersonSerializer()
+    Title = TitleSerializer()
     class Meta:
         model = Staff
         fields = ('__all__')
