e36ab340e9cba9878845aaec1606a8ae9826e996
emrah ince <emrahince@outlook.com>
2020-08-26 13:56:02 +0300

    rolewithpermission edit

--- apps/role/serializer.py
+++ apps/role/serializer.py
@@ -2,10 +2,15 @@ from rest_framework import serializers
 from .models import Role
 
 from ..authority.models import Authority
-from ..authority.serializer import AuthoritySerializer
-
 from ..permission.models import Permission
+from ..userrole.models import UserRole
+from ..account.models import Account
+from ..person.models import Person
+
+from ..authority.serializer import AuthoritySerializer
 from ..permission.serializer import PermissionSerializer
+
+
 
 class RoleSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
@@ -13,15 +18,62 @@ class RoleSerializer(serializers.ModelSerializer):
         model = Role
         fields = ('__all__')
 
+class PersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person       
+        fields = ('__all__')
+
+class AccountsDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = (
+            'id',
+            'email',
+            'PersonId',
+            'PersonName',
+            'PersonSurname'
+        )
+    PersonId = serializers.SerializerMethodField()
+    PersonName = serializers.SerializerMethodField()
+    PersonSurname = serializers.SerializerMethodField()
+
+    def get_PersonId(self,obj):
+        try:
+            person = Person.objects.get(Email = obj.email)
+            serializer = PersonSerializer(person)
+            return serializer.data['id']
+        except :
+            return None
+
+    def get_PersonName(self,obj):
+        try:
+            person = Person.objects.get(Email = obj.email)
+            serializer = PersonSerializer(person)
+            return serializer.data['Name']
+        except :
+            return None
+
+    def get_PersonSurname(self,obj):
+        try:
+            person = Person.objects.get(Email = obj.email)
+            serializer = PersonSerializer(person)
+            return serializer.data['Surname']
+        except :
+            return None
+             
+
 class RoleViewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
         fields = (
             'id',
             'Name',
-            'Permissions'
+            'Permissions',
+            'Users'
         )
     Permissions = serializers.SerializerMethodField()
+    Users = serializers.SerializerMethodField()
+
     def get_Permissions(self,obj):
         allPermission = []
         authoriyes = Authority.objects.filter(Role_id = obj.id)
@@ -30,6 +82,20 @@ class RoleViewSerializer(serializers.ModelSerializer):
             for permission in permissions:
                 allPermission.append(permission)
         return PermissionSerializer(allPermission,many=True).data
+
+    def get_Users(self,obj):
+        allAccounts = [] 
+        userRoles = UserRole.objects.filter(Role_id = obj.id)
+        for userrole in userRoles:
+            account = Account.objects.filter(id = userrole.Account_id)
+            for user in account:
+                allAccounts.append(user)
+        return AccountsDetailSerializer(allAccounts,many=True).data
+
+
+
+
+
 
 
         
