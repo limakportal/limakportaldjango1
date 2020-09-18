d1a3cd416fbec6e6cb27d02300349802d756d40e
emrah ince <emrahince@outlook.com>
2020-09-18 14:43:36 +0300

    UserRole tablosunda Organizations eklendi

--- apps/userrole/models.py
+++ apps/userrole/models.py
@@ -6,9 +6,10 @@ from ..role.models import Role
 class UserRole(models.Model):
     class Meta:
         db_table = 'UserRole'
-    Account = models.ForeignKey(Account,on_delete=models.CASCADE)
-    Role = models.ForeignKey(Role,on_delete=models.CASCADE)
+
+    Account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    Role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    Organizations = models.CharField(blank=True, null=True, max_length=100)
 
     def __str__(self):
         return self.Account.email
-
