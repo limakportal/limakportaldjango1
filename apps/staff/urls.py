09f69a20bf88ec661510264ee6b1289b6c58fd1a
emrah ince <emrahince@outlook.com>
2020-09-22 15:21:12 +0300

    getresponsiblestaff method eklendi giriş yapan kişinin sorumlu staffları getiren method ,token kontrolü eklendi

--- apps/staff/urls.py
+++ apps/staff/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import StaffAPIView , StaffDetails
+from .businesrules import GetResponsibleStaff
 
 
 urlpatterns = [ 
     path('staff', StaffAPIView.as_view()),
+    path('getresponsiblestaff', GetResponsibleStaff),
     path('staff/<int:id>', StaffDetails.as_view()),
 ]
