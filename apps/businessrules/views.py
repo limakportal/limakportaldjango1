8babbef0e0c82dd66093532f642e9edd54e9abbf
emrah ince <emrahince@outlook.com>
2020-09-22 11:45:22 +0300

    responsibleperson Token kontrolü eklendi

--- apps/businessrules/views.py
+++ apps/businessrules/views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -19,6 +20,7 @@ from ..dashboard.businesrules import ListResponsiblePersons
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def ResponsiblePersonDetails(request, id):
     response = {}
     try:
