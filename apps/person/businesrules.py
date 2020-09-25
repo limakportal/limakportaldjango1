edf0a3007b60748dd71e7f4df371a4fdaf83084e
emrah ince <emrahince@outlook.com>
2020-09-25 15:05:55 +0300

    PersonApprover düzenlendi

--- apps/person/businesrules.py
+++ apps/person/businesrules.py
@@ -1,50 +1,87 @@
-from rest_framework.response import Response
-from rest_framework import status
-
-from ..staff.models import Staff
-from ..organization.models import Organization
-from .models import Person
-from .serializer import PersonSerializer
-from ..personidentity.models import PersonIdentity
-from rest_framework.decorators import api_view
-from datetime import datetime
-from ..title.models import Title
 from datetime import datetime
 
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import Person
 from .serializer import PersonSerializer
+
+from .models import Person
 from ..organization.models import Organization
 from ..personidentity.models import PersonIdentity
 from ..staff.models import Staff
 from ..title.models import Title
+from ..account.models import Account
+from ..userrole.models import UserRole
 
 
 @api_view(['GET'])
 def PersonApprover(request, id):
+    dataArr = []
+    try:
+        organization_Arr = []
+        staff = Staff.objects.get(Person=id)
+        organization_Arr.append(staff.Organization_id)
+
+        try:
+            account_queryset = Account.objects.get(email=Person.objects.get(id=id).Email)
+            userRoles_queryset = UserRole.objects.filter(Account_id=account_queryset.id, Organizations__isnull=False)
+
+            if len(userRoles_queryset) > 0:
+                for u in userRoles_queryset:
+                    if len(u.Organizations) > 0:
+                        organizationIdArr = u.Organizations.split(",")
+                        for o in organizationIdArr:
+                            if o in organization_Arr:
+                                break
+                            organization_Arr.append(o)
+        except:
+            pass
+
+        for oa in organization_Arr:
+            data = GetPersonApprover2(oa)
+            dataArr.append(data)
+
+
+    except:
+        pass
+
+    newDataArr = []
+    for da in dataArr:
+        if da in newDataArr:
+            break
+        if da["FullName"] != "":
+            newDataArr.append(da)
+
+    return Response(newDataArr)
+
+
+def GetPersonApprover2(organizationId):
     global newstaff
     try:
-        staff = Staff.objects.get(Person=id)
-        organization = Organization.objects.get(id=staff.Organization.id)
+        organization = Organization.objects.get(id=organizationId)
         if organization.CanApproveRight:
             if organization.ManagerTitle is None:
-                return Response('Kişinin bağlı olduğu birimde yönetici bulunmamaktadır.',
-                                status=status.HTTP_404_NOT_FOUND)
+                data = {}
+                data['id'] = ""
+                data['FullName'] = ""
+                return data
 
             ystaff = Staff.objects.filter(Organization=organization.id, Title=organization.ManagerTitle.id)
 
             if len(ystaff) == 0:
-                return Response('Kişinin bağlı olduğu birimde yönetici bulunmamaktadır.',
-                                status=status.HTTP_404_NOT_FOUND)
+                data = {}
+                data['id'] = ""
+                data['FullName'] = ""
+                return data
             personel = Person.objects.get(id=ystaff[0].Person.id)
             if personel.id == id:
                 staffs = Staff.objects.filter(Organization=organization.UpperOrganization.id)
                 if len(staffs) == 0:
-                    return Response('Kişinin bağlı olduğu birimde yönetici bulunmamaktadır.',
-                                    status=status.HTTP_404_NOT_FOUND)
+                    data = {}
+                    data['id'] = ""
+                    data['FullName'] = ""
+                    return data
                 personel = Person.objects.get(id=staffs[0].Person.id)
                 serializer = PersonSerializer(personel)
                 data = {}
@@ -55,13 +92,16 @@ def PersonApprover(request, id):
             data = {}
             data['id'] = serializer.data['id']
             data['FullName'] = serializer.data['Name'] + ' ' + serializer.data['Surname']
-            return Response(data)
+            return data
         else:
             neworganization = Organization.objects.get(id=organization.UpperOrganization.id)
             if neworganization.CanApproveRight:
                 newstaff = Staff.objects.filter(Organization=neworganization.id, Title=neworganization.ManagerTitle.id)
                 if len(newstaff) == 0:
-                    return Response('Kişinin bağlı olduğum birimde yönetici bulunmamaktadır.',status=status.HTTP_404_NOT_FOUND)
+                    data = {}
+                    data['id'] = ""
+                    data['FullName'] = ""
+                    return data
                 personel = Person.objects.get(id=newstaff[0].Person.id)
                 if personel.id == id:
                     if organization.UpperOrganization.id is None:
@@ -69,31 +109,31 @@ def PersonApprover(request, id):
                         data = {}
                         data['id'] = serializer.data['id']
                         data['FullName'] = serializer.data['Name'] + ' ' + serializer.data['Surname']
-                        return Response(data)
+                        return data
                     staffs = Staff.objects.filter(Organization=organization.UpperOrganization.id)
                     if len(staffs) == 0:
-                        return Response('Kişinin bağlı olduğu birimde yönetici bulunmamaktadır.',status=status.HTTP_404_NOT_FOUND)
+                        data = {}
+                        data['id'] = ""
+                        data['FullName'] = ""
+                        return data
                     personel = Person.objects.get(id=staffs[0].Person.id)
                     serializer = PersonSerializer(personel)
                     data = {}
                     data['id'] = serializer.data['id']
                     data['FullName'] = serializer.data['Name'] + ' ' + serializer.data['Surname']
-                    return Response(data)
+                    return data
             serializer = PersonSerializer(personel)
             data = {}
             data['id'] = serializer.data['id']
             data['FullName'] = serializer.data['Name'] + ' ' + serializer.data['Surname']
-            return Response(data)
-            # staffs = Staff.objects.get(Organization = organization.UpperOrganization.id)
-            # personel = Person.objects.get(id=staffs.Person.id)
-            # serializer = PersonSerializer(personel)
-            # data = {}
-            # data['id'] = serializer.data['id']
-            # data['FullName'] = serializer.data['Name'] + ' ' + serializer.data['Surname']
-            # return Response(data)
+            return data
+
 
     except:
-        return Response("Kadro Bulunamadı", status=status.HTTP_404_NOT_FOUND)
+        data = {}
+        data['id'] = ""
+        data['FullName'] = ""
+        return data
 
 
 def GetPersonApprover(id):
@@ -118,7 +158,7 @@ def GetPersonApprover(id):
                     serializer = PersonSerializer(personel)
                     return serializer
                 except:
-                    return ""    
+                    return ""
             serializer = PersonSerializer(personel)
             return serializer
         else:
