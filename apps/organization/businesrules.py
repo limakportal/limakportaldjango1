acfca28de205f3bd60e19848446b76a3c018e299
kayaserhat <kayaserhat@users.noreply.github.com>
2020-10-04 22:59:55 +0300

    admin girerse tüm şirketlerin gelmesi

--- apps/organization/businesrules.py
+++ apps/organization/businesrules.py
@@ -2,11 +2,12 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from ..dashboard.businesrules import GetOrganizationAndLoweOrganization
+from ..dashboard.businesrules import GetOrganizationAndLoweOrganization, PersonPermissionControl
 
 from ..person.models import Person
 from ..staff.models import Staff
 from ..userrole.models import UserRole
+from ..organization.models import Organization
 
 from .serializer import OrganizationSerializer
 
@@ -19,15 +20,18 @@ def GetResponsibleOrganization(request):
         person_queryset = Person.objects.get(Email=account.email)
         staff_queryset = Staff.objects.get(Person_id=person_queryset.id)
         organizationArr = []
-        organizationArr = GetOrganizationAndLoweOrganization(staff_queryset.Organization_id, organizationArr)
+        if PersonPermissionControl(person_queryset.id, 'ADMIN'):
+            organizationArr = list(Organization.objects.all())
+        else:
+            organizationArr = GetOrganizationAndLoweOrganization(staff_queryset.Organization_id, organizationArr)
 
-        userRole_queryset = UserRole.objects.filter(Account_id=account.id, Organizations__isnull=False)
+            userRole_queryset = UserRole.objects.filter(Account_id=account.id, Organizations__isnull=False)
 
-        for ur in userRole_queryset:
-            if len(ur.Organizations) > 0:
-                organizationIdArr = ur.Organizations.split(",")
-                for o in organizationIdArr:
-                    organizationArr = GetOrganizationAndLoweOrganization(o, organizationArr)
+            for ur in userRole_queryset:
+                if len(ur.Organizations) > 0:
+                    organizationIdArr = ur.Organizations.split(",")
+                    for o in organizationIdArr:
+                        organizationArr = GetOrganizationAndLoweOrganization(o, organizationArr)
 
         organization_serializer_class = OrganizationSerializer(organizationArr, many=True)
     except:
